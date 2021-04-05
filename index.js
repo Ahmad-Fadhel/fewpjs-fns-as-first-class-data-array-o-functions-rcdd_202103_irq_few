66142da8ba7b5a189afbb6f2ca0e094116b5aca4
Ahmad Fadhel <ahmad.f.ameen@gmail.com>
2021-04-05 00:17:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function wakeDog(dogName, dogBreed) {
-  console.log(`Wake ${dogName} the ${dogBreed}`);
-  return `Wake ${dogName} the ${dogBreed}`;
+  console.log(`wake ${dogName} the ${dogBreed}`);
+  return `wake ${dogName} the ${dogBreed}`;
 }
 function leashDog(dogName, dogBreed) {
   console.log(`leash ${dogName} the ${dogBreed}`);
@@ -11,16 +11,16 @@ function walkToPark(dogName, dogBreed) {
   return `walk ${dogName} the ${dogBreed}`;
 }
 function throwFrisbee(dogName, dogBreed) {
-  console.log(`throw Frisbee ${dogName} the ${dogBreed}`);
-  return `throw Frisbee ${dogName} the ${dogBreed}`;
+  console.log(`Throw Frisbee ${dogName} the ${dogBreed}`);
+  return `Throw Frisbee ${dogName} the ${dogBreed}`;
 }
 function walkHome(dogName, dogBreed) {
   console.log(`walk Home ${dogName} the ${dogBreed}`);
-  return `walk Home ${dogName} the ${dogBreed}`;
+  return `walk home ${dogName} the ${dogBreed}`;
 }
 function unleashDog(dogName, dogBreed) {
   console.log(`unleash ${dogName} the ${dogBreed}`);
-  return `unleash ${dogName} the ${dogBreed}`;
+  return `Unleash ${dogName} the ${dogBreed}`;
 }
 
 let routine = [wakeDog, leashDog, walkToPark, throwFrisbee, walkHome, unleashDog];
